605e983dc072c6d5d77076e937e2031cb0ea25c8
Danish1999 <danish01031999@gmail.com>
2018-06-02 15:26:49 +0000

    Citrus Bringup

--- citrus.mk
+++ citrus.mk
@@ -20,11 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from z2_plus device
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
-# Inherit some common Cardinal stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+# Inherit some common Citrus stuff.
+$(call inherit-product, vendor/citrus/common.mk)
+
+# Inherit from our revision specific configuration
+$(call inherit-product, vendor/citrus/config/common_la.um.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cardinal_z2_plus
+PRODUCT_NAME := citrus_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_BRAND := ZUK
 PRODUCT_MODEL := Z2 Plus
